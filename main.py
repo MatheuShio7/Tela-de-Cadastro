c7873a7c7a00095a6b2135ea44104a56fd253781
Matheus Shiokawa Silva <matheus.pedro84@hotmail.com>
2023-06-03 23:07:00 -0300

    Adicionar codigo à cada cliente
    Criei uma branch para tentar adicionar códigos aos clientes

--- main.py
+++ main.py
@@ -61,13 +61,14 @@ class Relatorios:
 
 class Funcoes:
     def limpar_campos(self):
+        self.campo_code.delete(0, END)
         self.campo_nome.delete(0, END)
         self.campo_cpf.delete(0, END)
         self.campo_telefone.delete(0, END)
         self.campo_cidade.delete(0, END)
 
     def conectar_bd(self):
-        self.conectar = sqlite3.connect('clientes.bd')
+        self.conectar = sqlite3.connect('clientes_code.bd')
         self.cursor = self.conectar.cursor(); print('Conectando ao banco de dados')
     
     def desconectar_bd(self):
@@ -76,10 +77,11 @@ class Funcoes:
     def montar_tabela(self):
         self.conectar_bd()
         self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS clientes (
-                cpf INTEGER PRIMERY KEY,
+            CREATE TABLE IF NOT EXISTS clientes_code (
+                code INTEGER PRIMARY KEY,
+                cpf INTEGER,
                 nome_cliente CHAR(40) NOT NULL,
-                telefone INTEGER(20),
+                telefone INTEGER(20) NOT NULL,
                 cidade CHAR(40)
             );
         ''')
@@ -87,6 +89,7 @@ class Funcoes:
         self.desconectar_bd()
     
     def variaveis(self):
+        self.code = self.campo_code.get()
         self.cpf = self.campo_cpf.get()
         self.nome = self.campo_nome.get()
         self.telefone = self.campo_telefone.get()
@@ -95,8 +98,7 @@ class Funcoes:
     def cadastrar(self):
         self.variaveis()
         self.conectar_bd()
-
-        self.cursor.execute(''' INSERT INTO clientes (cpf, nome_cliente, telefone, cidade)
+        self.cursor.execute(''' INSERT INTO clientes_code (cpf, nome_cliente, telefone, cidade)
             VALUES (?, ?, ?, ?)''', (self.cpf, self.nome, self.telefone, self.cidade))
         self.conectar.commit()
         self.desconectar_bd()
@@ -106,7 +108,7 @@ class Funcoes:
     def select_lista(self):
         self.lista_clientes.delete(*self.lista_clientes.get_children())
         self.conectar_bd()
-        lista = self.cursor.execute(''' SELECT cpf, nome_cliente, telefone, cidade FROM clientes
+        lista = self.cursor.execute(''' SELECT code, cpf, nome_cliente, telefone, cidade FROM clientes_code
             ORDER BY nome_cliente ASC; ''')
         for i in lista:
             self.lista_clientes.insert('', END, values=i)
@@ -117,16 +119,17 @@ class Funcoes:
         self.lista_clientes.selection()
 
         for n in self.lista_clientes.selection():
-            col1, col2, col3, col4 = self.lista_clientes.item(n, 'values')
-            self.campo_cpf.insert(END, col1)
-            self.campo_nome.insert(END, col2)
-            self.campo_telefone.insert(END, col3)
-            self.campo_cidade.insert(END, col4)
+            col1, col2, col3, col4, col5 = self.lista_clientes.item(n, 'values')
+            self.campo_code.insert(END, col1)
+            self.campo_cpf.insert(END, col2)
+            self.campo_nome.insert(END, col3)
+            self.campo_telefone.insert(END, col4)
+            self.campo_cidade.insert(END, col5)
 
     def deletar_cliente(self):
         self.variaveis()
         self.conectar_bd()
-        self.cursor.execute('''DELETE FROM clientes WHERE cpf = ? ''', (self.cpf,))
+        self.cursor.execute('''DELETE FROM clientes_code WHERE cpf = ? ''', (self.cpf,))
         self.conectar.commit()
         self.desconectar_bd()
         self.limpar_campos()
@@ -135,8 +138,8 @@ class Funcoes:
     def alterar_info(self):
         self.variaveis()
         self.conectar_bd()
-        self.cursor.execute(''' UPDATE clientes SET nome_cliente = ?, telefone = ?, cidade = ?
-            WHERE cpf = ?''', (self.nome, self.telefone, self.cidade, self.cpf))
+        self.cursor.execute(''' UPDATE clientes_code SET cpf = ?, nome_cliente = ?, telefone = ?, cidade = ?
+            WHERE code = ?''', (self.cpf, self.nome, self.telefone, self.cidade, self.code))
         self.conectar.commit()
         self.desconectar_bd()
         self.select_lista()
@@ -186,19 +189,25 @@ class Interface(Funcoes, Relatorios):
         self.botao_limpar.place(relx=0.3,rely=0.5,relwidth=0.07, relheight=0.03)
 
     def textos(self):
+        self.texto_codigo = Label(self.janela, text='Cod.:', bg='#003060', fg='white', font=('verdana', 12))
+        self.texto_codigo.place(relx=0.148,rely=0.09)
+
         self.texto_nome = Label(self.janela, text='Nome Completo:', bg='#003060', fg='white', font=('verdana', 12))
-        self.texto_nome.place(relx=0.15,rely=0.17)
+        self.texto_nome.place(relx=0.148,rely=0.17)
 
         self.texto_cpf = Label(self.janela, text='CPF:', bg='#003060', fg='white', font=('verdana', 12))
-        self.texto_cpf.place(relx=0.15,rely=0.25)
+        self.texto_cpf.place(relx=0.148,rely=0.25)
 
         self.texto_telefone = Label(self.janela, text='Telefone:', bg='#003060', fg='white', font=('verdana', 12))
-        self.texto_telefone.place(relx=0.15,rely=0.33)
+        self.texto_telefone.place(relx=0.148,rely=0.33)
 
         self.texto_cidade = Label(self.janela, text='Cidade:', bg='#003060', fg='white', font=('verdana', 12))
-        self.texto_cidade.place(relx=0.15,rely=0.41)
+        self.texto_cidade.place(relx=0.148,rely=0.41)
 
     def campos(self):
+        self.campo_code = Entry(self.janela, font=('verdana', 10))
+        self.campo_code.place(relx=0.15,rely=0.12, relwidth=0.03, relheight=0.03)
+
         self.campo_nome = Entry(self.janela, font=('verdana', 10))
         self.campo_nome.place(relx=0.15,rely=0.20, relwidth=0.25, relheight=0.03)
         
@@ -212,19 +221,21 @@ class Interface(Funcoes, Relatorios):
         self.campo_cidade.place(relx=0.15, rely=0.44, relwidth=0.25, relheight=0.03)
     
     def lista(self):
-        self.lista_clientes = ttk.Treeview(self.frame1, height=3, column=('coluna1', 'coluna2', 'coluna3', 'coluna4'))
+        self.lista_clientes = ttk.Treeview(self.frame1, height=3, column=('coluna1', 'coluna2', 'coluna3', 'coluna4', 'coluna5'))
 
         self.lista_clientes.heading('#0', text='')
-        self.lista_clientes.heading('#1', text='CPF')
-        self.lista_clientes.heading('#2', text='Nome')
-        self.lista_clientes.heading('#3', text='Telefone')
-        self.lista_clientes.heading('#4', text='Cidade')
+        self.lista_clientes.heading('#1', text='Código')
+        self.lista_clientes.heading('#2', text='CPF')
+        self.lista_clientes.heading('#3', text='Nome')
+        self.lista_clientes.heading('#4', text='Telefone')
+        self.lista_clientes.heading('#5', text='Cidade')
 
         self.lista_clientes.column('#0', width=1)
-        self.lista_clientes.column('#1', width=125)
-        self.lista_clientes.column('#2', width=125)
-        self.lista_clientes.column('#3', width=125)
-        self.lista_clientes.column('#4', width=125)
+        self.lista_clientes.column('#1', width=50)
+        self.lista_clientes.column('#2', width=75)
+        self.lista_clientes.column('#3', width=150)
+        self.lista_clientes.column('#4', width=100)
+        self.lista_clientes.column('#5', width=100)
         
         self.lista_clientes.place(relx=0.0001, rely=0.0001, relheight=1, relwidth=1)
 
